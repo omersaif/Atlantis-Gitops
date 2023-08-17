53728ef386e862b840cb1a26bf8a39c1e64c8699
Ashish_Sarawad <92158835+Ashish-sarawad@users.noreply.github.com>
2023-08-17 14:20:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "atlantis-instance" {
   ami                    = "ami-08a52ddb321b32a8c"
-  instance_type          = "var.instance_type"
+  instance_type          = var.instance_type
 
 
   tags = {
-    Name = "var.instance_name"
+    Name = var.instance_name
   }
 }
